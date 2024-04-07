7ea0fe33fe72e8e03599b6fa982ea3d7e61bc03a
roughnut <jeremy@writever.com.au>
2024-04-08 08:11:21 +1000

    commit completed files and assets

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # 05 Third-Party APIs: Task Board
 
+Bootcamp challenge to modify starting HTML, CSS and JavaScript code to make a simple Kanban task board functional.
+
 ## Your Task
 
-Create a simple task board application that allows a team to manage project tasks by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+Create a simple task board application that allows a team to manage project tasks in the browser with dynamically updated features using jQuery and the [Day.js](https://day.js.org/en/) API library.
 
-You'll need to use the [Day.js](https://day.js.org/en/) library to work with dates. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
+[User Story](#user-story) and [Acceptance Criteria](#acceptance-criteria) are below.
+
+## Screenshot
+![app screenshot](./assets/images/app-screenshot.png)
+
+## Usage
+
+The site is deployed to Github pages: [Github Pages URL](https://roughnut.github.io/kanban-task-board/ "link to deployed site")
 
 ## User Story
 
@@ -33,68 +42,3 @@ THEN the task is removed from the task board and will not be added back after re
 WHEN I refresh the page
 THEN the saved tasks persist
 ```
-
-The following animation demonstrates the application functionality:
-
-![A user adds three tasks to the task board and changes the state of two of them to in progress and then completion. The user then deletes the two cards in the done column.](./Assets/05-third-party-apis-homework-demo.gif)
-
-## Grading Requirements
-
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria:
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-  * Uses the Day.js library to work with dates
-
-### Deployment: 32%
-
-* Application deployed at live URL
-
-* Application loads with no errors
-
-* Application GitHub URL submitted
-
-* GitHub repo contains application code
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate
-
-* Application user interface style is clean and polished
-
-* Application resembles the mock-up functionality provided in the Challenge instructions
-
-### Repository Quality: 13%
-
-* Repository has a unique name
-
-* Repository follows best practices for file structure and naming conventions
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages
-
-* Repository contains quality README file with description, screenshot, and link to deployed application
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application
-
-* The URL of the GitHub repository, with a unique name and a README describing the project
-
-- - -
-© 2024 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,71 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
-
+    let taskList = JSON.parse(localStorage.getItem("tasks"));
+    let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Generate a unique task id
-function generateTaskId() {
+    function generateTaskId() {
   nextId++;
   return nextId;
 }
 
 // Render tasks in appropriate lanes
-function createTaskCard(task) {
+    function createTaskCard(task) {
   // Create a new card
-  const newTaskCard = $("<div>");
-  newTaskCard.addClass("card my-2 task-card todo-card");
-  newTaskCard.attr("id", task.id);
+    const newTaskCard = $("<div>");
+    newTaskCard.addClass("card my-2 task-card todo-card");
+    newTaskCard.attr("id", task.id);
 
   // Create a card header
-  const newTaskCardHeader = $("<div>");
-  newTaskCardHeader.addClass("card-header");
-  newTaskCardHeader.text(task.name);
+    const newTaskCardHeader = $("<h4>");
+    newTaskCardHeader.addClass("card-header");
+    newTaskCardHeader.text(task.name);
+
+  // use Day.js to calculate the number of days until the due date
+    const today = dayjs();
+
+  //convert the due date to a dayjs object
+    const dueDate = dayjs(task.dueDate);
+
+  //calculate the difference between the due date and today
+    const daysUntilDue = dueDate.diff(today, "day");
+
+  // Apply bootstrap classes based on the number of days until the due date
+    if (daysUntilDue < 0) {
+        newTaskCard.addClass("bg-danger text-white");
+    } else if (daysUntilDue < 7) {
+        newTaskCard.addClass("bg-warning");
+    } 
 
   // append the due date to the card
-  const newTaskCardDueDate = $("<div>");
-  newTaskCardDueDate.addClass("card-header");
-  newTaskCardDueDate.text(task.dueDate);
+    const newTaskCardDueDate = $("<p>");
+    newTaskCardDueDate.addClass("card-body");
+    newTaskCardDueDate.text(
+        `Due: ${dueDate.format("DD-MM-YYYY")} - Days left: ${daysUntilDue}`
+    );
 
   // Create a card body
-  const newTaskCardBody = $("<div>");
-  newTaskCardBody.addClass("card-body");
-  newTaskCardBody.text(task.description);
+    const newTaskCardBody = $("<p>");
+    newTaskCardBody.addClass("card-body");
+    newTaskCardBody.text(task.description);
 
   // Create a delete button
-  const taskCardDeleteButton = $("<button>");
-  taskCardDeleteButton.addClass("btn btn-danger");
-  taskCardDeleteButton.text("Delete");
-  taskCardDeleteButton.click(handleDeleteTask);
+    const taskCardDeleteButton = $("<button>");
+    taskCardDeleteButton.addClass("btn btn-danger delete-button");
+    taskCardDeleteButton.text("Delete");
+    taskCardDeleteButton.click(handleDeleteTask);
 
   // Append the card header and body to the card
-  newTaskCard.append(newTaskCardHeader);
-  newTaskCard.append(newTaskCardDueDate);
-  newTaskCard.append(newTaskCardBody);
-  newTaskCard.append(taskCardDeleteButton);
+    newTaskCard.append(newTaskCardHeader);
+    newTaskCard.append(newTaskCardDueDate);
+    newTaskCard.append(newTaskCardBody);
+    newTaskCard.append(taskCardDeleteButton);
 
-  // Append the card to the appropriate lane
+// Append the card to the appropriate lane
     $(`#${task.lane}`).append(newTaskCard);
-
-
 }
 
 // Function to render the task list and make cards sortable
-function renderTaskList() {
+    function renderTaskList() {
   if (taskList) {
     taskList.forEach((task) => {
       createTaskCard(task);
@@ -66,22 +81,21 @@ function renderTaskList() {
 }
 
 // Update the task status in local storage when a card is dropped into a new lane
-function handleDrop(event, ui) {
-    const task = ui.item;
-    const lane = task.parent().attr("id");
+    function handleDrop(event, ui) {
+  const task = ui.item;
+  const lane = task.parent().attr("id");
 
-    // Update the task lane in the task list
-    const taskId = task.attr("id");
-    console.log(taskId);
-    const taskIndex = taskList.findIndex((task) => task.id == taskId);
-    taskList[taskIndex].lane = lane;
-    console.log(taskList[taskIndex].lane);
-    localStorage.setItem("tasks", JSON.stringify(taskList));
-
+  // Update the task lane in the task list
+  const taskId = task.attr("id");
+  console.log(taskId);
+  const taskIndex = taskList.findIndex((task) => task.id == taskId);
+  taskList[taskIndex].lane = lane;
+  console.log(taskList[taskIndex].lane);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
 }
 
-// Todo: create a function to handle adding a new task
-function handleAddTask(event) {
+// Function to handle adding a new task
+    function handleAddTask(event) {
   event.preventDefault();
 
   // Get form data
@@ -89,7 +103,13 @@ function handleAddTask(event) {
   const taskDueDate = $("#taskDueDate").val();
   const taskDescription = $("#taskDescription").val();
 
-  // create a task object
+  // Check if all fields have been completed
+  if (!taskName || !taskDueDate || !taskDescription) {
+    alert("Please complete all input fields.");
+    return;
+  }
+
+  // create a task object and generate a unique task id
   const task = {
     id: generateTaskId(),
     name: taskName,
@@ -97,9 +117,8 @@ function handleAddTask(event) {
     description: taskDescription,
     lane: "todo-lane",
   };
-  console.log(task);
+
   // Add the new task to your task list
-  
   createTaskCard(task);
 
   // If no tasks exist, set the task list to an empty array and set nextId to 1
@@ -112,38 +131,52 @@ function handleAddTask(event) {
   localStorage.setItem("tasks", JSON.stringify(taskList));
   localStorage.setItem("nextId", JSON.stringify(nextId));
 
+  // Clear the input fields
+  $("#taskName").val("");
+  $("#taskDueDate").val("");
+  $("#taskDescription").val("");
+
   // Close the dialog
   $("#addTaskModal").dialog("close");
 }
 
-// Todo: create a function to handle deleting a task
-function handleDeleteTask(event) {}
+// Function to handle deleting a task
+    function handleDeleteTask(event) {
+    // get the delete button parent
+        const taskCardToDelete = $(event.target).closest(".task-card");
+    // get the parent id
+        const taskId = taskCardToDelete.attr("id");
+    // Find the task index in the task list
+        const taskIndex = taskList.findIndex((task) => task.id == taskId);
+    // Remove the task from the task list
+        taskList.splice(taskIndex, 1);
+    // Delete the task card from the DOM
+        taskCardToDelete.remove();
+    // Update localStorage
+        localStorage.setItem("tasks", JSON.stringify(taskList));
+}
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
+// On page load
+    $(document).ready(function () {
   // Render the task list from localStorage
-  renderTaskList();
+    renderTaskList();
 
   //Initialise the add task modal
-  $("#addTaskModal").dialog({
+    $("#addTaskModal").dialog({
     autoOpen: false,
     modal: true,
   });
 
   // Add event listener for the add task button
-  $("#addTaskButton").click(function (event) {
+    $("#addTaskButton").click(function (event) {
     event.preventDefault();
     $("#addTaskModal").dialog("open");
-    $("#taskDueDate").datepicker(
-        {
-            dateFormat: "dd-mm-yy",
-        }
-    );
+    $("#taskDueDate").datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
   });
 
   // Add listener for the form submission
-  $("#taskForm").on("submit", handleAddTask);
-
-  // Handle delete task button click
-
+    $("#taskForm").on("submit", handleAddTask);
 });
